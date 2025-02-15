8c7fcf126d5a257f893e27d9356171ec26c49af0
暇な人 <yuuta4083@gmail.com>
2025-02-15 13:09:09 +0900

    最初のCogの追加

---
+++ cogs/global.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+import discord
+
+class GlobalCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        print(f"init -> GlobalCog")
+
+
+
+async def setup(bot):
+    await bot.add_cog(GlobalCog(bot))
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import discord
 from discord.ext import commands
+import os
 
-bot = commands.Bot(command_prefix="Prefix", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="pa!", intents=discord.Intents.all(), help_command=None)
 
 @bot.event
 async def on_ready():
     print('ログインしました')
+    await bot.change_presence(activity=discord.Game(name=f"{len(bot.guilds)}サーバー"))
 
 @bot.event
 async def on_message(message: discord.Message):
@@ -13,13 +15,26 @@ async def on_message(message: discord.Message):
         return
 
     if message.content == "おはよう":
-        await message.reply("おはよう！")
+        await message.reply("おはよ！")
+        return
+    elif message.content == "おやすみ":
+        await message.reply("おやすみ！ねんねしてね！")
         return
 
     bot.process_commands(message)
 
-@bot.command(name="hello")
-async def hello(ctx: commands.Context):
-    await ctx.reply("Hello!")
+@bot.event
+async def setup_hook() -> None:
+    for cog in os.listdir("cogs"):
+        if cog.endswith(".py"):
+            await bot.load_extension(f"cogs.{cog[:-3]}")
+    await bot.tree.sync()
+    print("Cogの読み込みとスラッシュコマンドの同期に成功しました。")
+
+@bot.command(name="reload")
+@bot.is_owner()
+async def reload(ctx: commands.Context, cogname: str):
+    await bot.reload_extension(f"cogs.{cogname}")
+    await ctx.reply("再読み込みしました。")
 
 bot.run("Token")
