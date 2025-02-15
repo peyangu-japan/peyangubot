9d08bf87c6fd23c81b38147c4da1d5a8a10fd4b9
暇な人 <yuuta4083@gmail.com>
2025-02-15 19:00:30 +0900

    サーバーリストの追加

--- cogs/admin.py
+++ cogs/admin.py
@@ -7,5 +7,10 @@ class AdminCog(commands.Cog):
         self.bot = bot
         print(f"init -> AdminCog")
 
+    @commands.command(name="guilds_list")
+    @commands.is_owner()
+    async def guilds_list(self, ctx: commands.Context):
+        await ctx.reply(f"サーバーリスト:\n{"\n".join([f"{g.name} - {g.id}" for g in self.bot.guilds])}")
+
 async def setup(bot):
     await bot.add_cog(AdminCog(bot))
